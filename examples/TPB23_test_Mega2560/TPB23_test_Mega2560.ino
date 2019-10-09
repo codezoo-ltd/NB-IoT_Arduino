664e3f98fddf7c2cd3f41d7e2d67ddae98709070
Rooney Jang <rooney.jang@codezoo.co.kr>
2019-10-10 01:30:18 +0900

    Add Mega2560 NB-IoT TestCode
    Signed-off-by: Rooney Jang <rooney.jang@codezoo.co.kr>

---
+++ examples/TPB23_test_Mega2560/TPB23_test_Mega2560.ino
@@ -0,0 +1,241 @@
+#include "TPB23.h"
+
+#define DebugSerial Serial
+#define NBSerial  Serial3
+#define NBReset   A1
+
+TPB23 TPB23(NBSerial, DebugSerial);
+
+void setup() {
+	// put your setup code here, to run once:
+  pinMode(NBReset, OUTPUT);
+	NBSerial.begin(9600);
+	DebugSerial.begin(9600);
+
+  digitalWrite(NBReset, HIGH);
+  delay(500);
+  digitalWrite(NBReset, LOW);
+  delay(1000);
+
+
+	/* TPB23 Module Initialization */
+	if(TPB23.init()){
+		DebugSerial.println("TPB23 Module Error!!!");
+	}
+
+	/* Network Regsistraiton Check */
+	while(TPB23.canConnect() != 0){
+		DebugSerial.println("Network not Ready !!!");
+		delay(2000);
+	}
+
+	/* Network QoS Report to LGU+ (No-charge packet) */
+	if(TPB23.reportDevice() == 0)
+	{
+		DebugSerial.println("TPB23 Device Report Success!!!");
+	}    
+
+	delay(2000);
+
+	DebugSerial.println("TPB23 Module Ready!!!");
+}
+
+int value = 0;
+
+void loop() {
+
+#if 0
+	/* 1. CGMR, IMEI, CIMI, servingCell */
+	char szCGMR[20];
+
+	if(TPB23.getCGMR(szCGMR, sizeof(szCGMR)) == 0)
+	{
+		DebugSerial.print("TPB23 CGMR : ");
+		DebugSerial.println(szCGMR);
+	}
+
+	char szIMEI[16];
+	if(TPB23.getIMEI(szIMEI, sizeof(szIMEI)) == 0)
+	{
+		DebugSerial.print("TPB23 IMEI : ");
+		DebugSerial.println(szIMEI);
+	}    
+
+	char szCIMI[16];
+
+	if(TPB23.getCIMI(szCIMI, sizeof(szCIMI)) == 0)
+	{
+		DebugSerial.print("CIMI : ");
+		DebugSerial.println(szCIMI);
+	}
+
+	char servingCell[10];
+	if(TPB23.getServingCell(servingCell) == 0)
+	{
+		DebugSerial.print("ServingCell : ");
+		DebugSerial.print(servingCell);
+		DebugSerial.println("");
+	}
+	else
+	{
+		DebugSerial.println("ServingCell don't read");
+	}
+#endif
+
+#if 0
+	/* 2. RSSI, SignalPower, SNR */
+
+	/* Get CSQ to convert RSSI */
+	int rssi;
+
+	if(TPB23.getCSQ(&rssi) == 0)
+	{
+		DebugSerial.print("RSSI : ");
+		DebugSerial.print(rssi);
+		DebugSerial.println("");
+	}
+
+	/* Get Signal Power */
+	int sPower;
+
+	if(TPB23.getSignalPower(&sPower) == 0)
+	{
+		DebugSerial.print("Signal Power : ");
+		DebugSerial.print(sPower);
+		DebugSerial.println("");
+	}
+
+	/* Get SNR */
+	int snR;
+
+	if(TPB23.getSnr(&snR) == 0)
+	{
+		DebugSerial.print("SNR : ");
+		DebugSerial.print(snR);
+		DebugSerial.println("");
+	}
+#endif
+
+#if 0
+	/* 3. CFUN setting 0: disable, 1: Full Function */
+	if(value == 0)
+		value = 1;
+	else
+		value = 0;
+
+	if(TPB23.setCFUN(value) == 0)
+		DebugSerial.println("CFUN set OK");
+
+	delay(2000);
+
+	/* Network Check */
+	if(TPB23.getCFUN(&value) == 0)
+	{
+		DebugSerial.print("CFUN get : ");
+		DebugSerial.print(value);
+		DebugSerial.println("");
+	}  
+#endif
+
+#if 0      
+	/* 4. UDP Socket Text Echo Test */
+	char _IP[] = "195.34.89.241"; //Ublox UDP Test Server
+	int  _PORT = 7;
+
+	char sendBuffer[] = "Hello CodeZoo!!!";
+	char recvBuffer[32];
+
+	/* A number in the range 0-65535 except 5683 */
+	if(TPB23.socketCreate(1024) == 0)
+		DebugSerial.println("Socket Create!!!");
+
+	/* Socket Send */
+	if(TPB23.socketSend(_IP, _PORT, sendBuffer) == 0){
+		DebugSerial.print("[UDP Send] >>  ");
+		DebugSerial.println(sendBuffer);
+	}
+	else
+		DebugSerial.println("Send Fail!!!");
+
+	/* Socket Recv */
+	if(TPB23.socketRecv(recvBuffer, sizeof(recvBuffer), 5000) == 0){
+		DebugSerial.print("[UDP Recv] >>  ");
+		DebugSerial.println(recvBuffer);
+	}else{
+		DebugSerial.println("Recv Fail!!!");
+	}
+
+	/* Socket Close */
+	if(TPB23.socketClose() == 0)
+		DebugSerial.println("Socket Close!!!");
+
+	/* NB-IoT Network Detatch */
+	if(TPB23.setCFUN(0) == 0)
+		DebugSerial.println("NB-IoT Network Detach");
+
+	delay(2000);        
+
+	while(1){
+		delay(100);
+	}
+
+#endif
+
+#if 1
+	/* 5. UDP Socket Binary Echo Test */
+	char _IP[] = "195.34.89.241"; //Ublox UDP Test Server
+	int  _PORT = 7;
+
+	char sendBuffer[32];
+	char recvBuffer[32];
+
+	memset(sendBuffer, 0, sizeof(sendBuffer));
+	memset(recvBuffer, 0, sizeof(recvBuffer));
+
+	for(int k=0; k<32; k++)
+		sendBuffer[k] = k;
+
+	/* A number in the range 0-65535 except 5683 */
+	if(TPB23.socketCreate(1024) == 0) 
+		DebugSerial.println("Socket Create!!!");    
+
+	if( !TPB23.socketSend(_IP, _PORT, sendBuffer, sizeof(sendBuffer)) )
+		DebugSerial.println("UDP Binary Data Send!!!");
+	else
+		DebugSerial.println("Send Fail!!!");
+
+
+	if( !TPB23.socketRecv(recvBuffer, sizeof(recvBuffer), 5000) ){  
+		DebugSerial.print("[UDP Recv] : ");
+
+		for(int j=0; j<32; j++){
+			DebugSerial.print(recvBuffer[j],DEC);
+			DebugSerial.print(',');
+		}
+		DebugSerial.println();
+	}
+	else{
+		DebugSerial.println("Recv Fail!!!");
+	}
+
+	if( !memcmp(sendBuffer, recvBuffer, sizeof(sendBuffer)) )
+		DebugSerial.println("Send & Recv OK!!!");
+
+	if( !TPB23.socketClose() ){
+		DebugSerial.println("Socket Close!!!");
+	}
+
+	/* NB-IoT Network Detatch */
+	if(TPB23.setCFUN(0) == 0)
+		DebugSerial.println("NB-IoT Network Detach");
+
+	delay(2000);        
+
+	while(1){
+		delay(100);
+	}
+
+#endif
+
+	delay(10000);      
+}
